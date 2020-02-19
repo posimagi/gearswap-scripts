780d131b9795df18eb074f7de4185b75b8a73904
posimagi <posimagi@gmail.com>
2020-02-19 01:55:06 -0800

    2020-02-19 update

--- cor/ws-leadensalute.lua
+++ cor/ws-leadensalute.lua
@@ -1,14 +1,7 @@
 sets.ws.leadensalute={
     head="Pixie Hairpin +1",
-    body="Meg. Cuirie +2",
-    hands="Meg. Gloves +2",
-    legs={ name="Herculean Trousers", augments={'Haste+1','"Dbl.Atk."+4','"Treasure Hunter"+1','Accuracy+17 Attack+17','Mag. Acc.+16 "Mag.Atk.Bns."+16',}},
-    feet="Mummu Gamash. +2",
-    neck="Fotia Gorget",
+    neck="Sanctity Necklace",
     waist="Eschan Stone",
     left_ear="Hecate's Earring",
-    right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +250',}},
-    left_ring="Mujin Band",
-    right_ring="Archon Ring",
-    back="Gunslinger's Cape",
+    left_ring="Archon Ring",
 }
