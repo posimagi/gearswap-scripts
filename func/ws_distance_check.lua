8af2041ae97cde144961bddfc7c8610882b42378
Posimagi <posimagi@gmail.com>
2021-04-30 18:10:44 -0700

    improvements to ws_distance_check

--- func/ws_distance_check.lua
+++ func/ws_distance_check.lua
@@ -1,11 +1,11 @@
 _RANGE_MULTIPLIER = 1.642276421172564
 _real_precast = precast
 function precast(spell, position)
-	if spell.type == "WeaponSkill" then
-		if spell.target.distance > (spell.range * _RANGE_MULTIPLIER + spell.target.model_size) then
-			add_to_chat(8, spell.name.." aborted due to target out of range.")
-			cancel_spell()
-		end
+	if spell.type == "WeaponSkill" and
+	   spell.target.distance > (spell.range * _RANGE_MULTIPLIER + spell.target.model_size) then
+		add_to_chat(8, spell.name.." aborted due to target out of range.")
+		cancel_spell()
+		return
 	end
 	_real_precast(spell, position)
 end
